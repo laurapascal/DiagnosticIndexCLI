61f482c92957d345ec33746cc03f959bb3cce863
Laura PASCAL <laurajulia.pascal@gmail.com>
2016-05-24 15:26:52 -0400

    ENH: Improvement of the CLI saveModel
    The reference, in order to create the shape model (hdf5 file), is now the mean of all the vtk files computed previously.

--- saveModel/saveModel.cxx
+++ saveModel/saveModel.cxx
@@ -8,6 +8,7 @@
 #include "vtkPolyDataReader.h"
 #include "saveModelCLP.h"
 #include <fstream>
+#include "vtkCellArray.h"
 
 using namespace statismo;
 typedef vtkStandardMeshRepresenter RepresenterType;
@@ -15,11 +16,11 @@ typedef DataManager<vtkPolyData> DataManagerType;
 typedef PCAModelBuilder<vtkPolyData> ModelBuilderType;
 typedef StatisticalModel<vtkPolyData> StatisticalModelType;
 
-vtkPolyData* loadVTKPolyData(const std::string& filename) {
-    vtkPolyDataReader* reader = vtkPolyDataReader::New();
+vtkSmartPointer<vtkPolyData> loadVTKPolyData(const std::string& filename) {
+    vtkSmartPointer<vtkPolyDataReader> reader = vtkSmartPointer<vtkPolyDataReader>::New();
     reader->SetFileName(filename.c_str());
     reader->Update();
-    vtkPolyData* pd = vtkPolyData::New();
+    vtkSmartPointer<vtkPolyData> pd = vtkSmartPointer<vtkPolyData>::New();
     pd->ShallowCopy(reader->GetOutput());
     return pd;
 }
@@ -28,7 +29,63 @@ int main(int argc, char ** argv)
 {
     PARSE_ARGS;
 
-    vtkSmartPointer<vtkPolyData> reference = loadVTKPolyData(vtkfilelist[0]);
+    if(argc < 7)
+    {
+        std::cout << "Usage " << argv[0] << " [--groupnumber <int>] [--vtkfilelist <std::vector<std::string>>] [--resultdir <std::string>]" << std::endl;
+        return 1;
+    }
+
+    // Average of all the VTK meshes
+    //   Mean of the 3 coordinates
+    vtkSmartPointer<vtkPolyData> polydata0 = loadVTKPolyData(vtkfilelist[0]);
+    int numPts = loadVTKPolyData(vtkfilelist[0])->GetPoints()->GetNumberOfPoints();
+    vtkSmartPointer<vtkCellArray> verts = polydata0->GetVerts();
+    vtkSmartPointer<vtkCellArray> lines = polydata0->GetLines();
+    vtkSmartPointer<vtkCellArray> polys = polydata0->GetPolys();
+    vtkSmartPointer<vtkCellArray> strips = polydata0->GetStrips();
+
+    vtkSmartPointer<vtkPolyData> polydata_MeanGroup = vtkSmartPointer<vtkPolyData>::New();
+    vtkSmartPointer<vtkPoints> points = vtkSmartPointer<vtkPoints>::New();
+    points = polydata0->GetPoints();
+    for(int meshID = 1; meshID < vtkfilelist.size(); meshID++)
+    {
+        vtkSmartPointer<vtkPolyData> polydata = vtkSmartPointer<vtkPolyData>::New();
+        polydata = loadVTKPolyData(vtkfilelist[meshID]);
+        for(int ptID = 0; ptID < numPts; ptID++)
+        {
+            double coord[3];
+            polydata->GetPoint(ptID, coord);
+            double sum[3];
+            points->GetPoint(ptID, sum);
+            for(int dim = 0; dim < 3; dim++)
+            {
+                sum[dim] = sum[dim] + coord[dim];
+            }
+            points->InsertPoint(ptID, sum);
+        }
+    }
+    double mean[3];
+    for(int ptID = 0; ptID < numPts; ptID++)
+    {
+        for(int dim = 0; dim < 3; dim++)
+        {
+            double sum[3];
+            points->GetPoint(ptID, sum);
+            mean[dim] = sum[dim]/vtkfilelist.size();
+        }
+        points->InsertPoint(ptID, mean);
+    }
+
+    //   Creation of the polydata of the mean of the given group
+    polydata_MeanGroup->SetPoints(points);
+    polydata_MeanGroup->SetVerts(verts);
+    polydata_MeanGroup->SetLines(lines);
+    polydata_MeanGroup->SetPolys(polys);
+    polydata_MeanGroup->SetStrips(strips);
+
+    // Creation of the shape model
+    vtkSmartPointer<vtkPolyData> reference = polydata_MeanGroup;
+
     boost::scoped_ptr<RepresenterType> representer(RepresenterType::Create(reference));
     boost::scoped_ptr<DataManagerType> dataManager(DataManagerType::Create(representer.get()));
     for(int j = 0; j < vtkfilelist.size(); j++)
@@ -37,11 +94,11 @@ int main(int argc, char ** argv)
     }
     boost::scoped_ptr<ModelBuilderType> modelBuilder(ModelBuilderType::Create());
     boost::scoped_ptr<StatisticalModelType> model(modelBuilder->BuildNewModel(dataManager->GetData(), 0.01));
-    
+
     // Once we have built the model, we can save in the directory choosen by the user.
     std::string H5File = resultdir + "/G" + std::to_string(groupnumber) + ".h5";
     model->Save(H5File);
-    std::cout << "Successfully saved shape model as " << "model.h5" << std::endl;
-    
+    std::cout << "Successfully saved shape model as " << "G" << groupnumber << ".h5" << std::endl;
+
     return 0;
 }
